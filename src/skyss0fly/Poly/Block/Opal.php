58624de02726fd4fb55e5901ea681d805ca3027e
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-12-24 10:36:38 +1000

    Create Opal.php

---
+++ src/skyss0fly/Poly/Block/Opal.php
@@ -0,0 +1,11 @@
+<?php
+namespace Poly\Block;
+
+use customiesdevs\customies\block\CustomiesBlockFactory;
+use pocketmine\block\BlockBreakInfo;
+use pocketmine\block\BlockIdentifier;
+use Poly\Block\BaseBlock;
+
+final class Opal extends BaseBlock {
+  
+}
